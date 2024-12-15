f1350ce6dde8505912f44bd521bb046e6fafd1ef
Lorena Mekaj <lorenamekaj@gmail.com>
2024-12-15 02:22:18 +0100

    Initial Login

---
+++ digitalwallet/App.tsx
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useAuth, AuthProvider } from './app/context/AuthContext';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './app/screens/Home';
+import Login from './app/screens/Login';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <Layout></Layout>
+    </AuthProvider>
+  );
+}
+
+export const Layout = () => {
+
+  const { authState, onLogout } = useAuth();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        { authState?.authenticated ? (
+          <Stack.Screen 
+            name="Home" 
+            component={Home}
+            options={{
+              headerRight: () => <Button onPress={onLogout} title="Sign Out" />, 
+            }}
+          ></Stack.Screen>
+        ) : (
+          <Stack.Screen name="Login" component={Login}></Stack.Screen>
+        ) }
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
